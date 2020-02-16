f4be5b764c817597c779f21e1147e26f839fb301
AtlasGeek7 <khalid.tissire@gmail.com>
2020-02-16 03:42:54 +0000

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+
+require_relative "../lib/cli.rb"
+require_relative "../lib/scraper.rb"
+require_relative "../lib/song.rb"
+require_relative "../lib/band.rb"
---
+++ lib/scraper.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Scraper
+  attr_accessor :doc
+  def scrape_init
+    html = open('https://www.rollingstone.com/music/music-lists/500-greatest-songs-of-all-time-151127/?list_page=10#list-item-50')
+    doc = Nokogiri::HTML(html)
+    scrape_list
+  end
+
+  def scrape_list
+    list = {}
+    doc_list = @doc.search('h3.c-list__title')
+    list_detail = @doc.search('div.c-list__lead.c-content')
+    list_arr = []
+    detail_arr = []
+
+    50.times { |i|
+      list_arr << doc_list[i].text.chomp.strip
+      detail_arr << list_detail[i+1].text.chomp.strip
+      list[(i-50).abs] = list_arr[i].split(',')
+  
+  
+  
+  
+  
+     }
+
+
+
+  end
+end
